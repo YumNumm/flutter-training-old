9b2af2371684d176a433182804b2076cabca40a5
Ryotaro Onoue <73390859+YumNumm@users.noreply.github.com>
2023-02-23 01:04:14 +0900

    Session 5: エラーハンドリング (#23)
    * fix: ci
    slangの多言語化対応はしていないので消去
    
    * add: delayedActionMixin
    
    * fix: 画面遷移をDelayedActionMixinを用いるように変更
    
    * add: エラーハンドリング

--- lib/ui/view/weather_result_view/weather_result_viewmodel.dart
+++ lib/ui/view/weather_result_view/weather_result_viewmodel.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_training/model/enum/weather_category.dart';
 import 'package:flutter_training/model/weather_result_view/weather_result_state.dart';
+import 'package:flutter_training/ui/component/error_dialog.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 class WeatherResultViewModel extends StateNotifier<WeatherResultState> {
@@ -8,6 +10,7 @@ class WeatherResultViewModel extends StateNotifier<WeatherResultState> {
 
   final _api = YumemiWeather();
 
+  @Deprecated('fetchThrowsWeatherを使ってください')
   void fetchWeather() {
     final result = _api.fetchSimpleWeather();
     final category = WeatherCategory.fromString(result);
@@ -15,6 +18,30 @@ class WeatherResultViewModel extends StateNotifier<WeatherResultState> {
       category: category,
     );
   }
+
+  void fetchThrowsWeather() {
+    final result = _api.fetchThrowsWeather('area');
+    final category = WeatherCategory.fromString(result);
+    state = state.copyWith(
+      category: category,
+    );
+  }
+
+  Future<void> showErrorDialog({
+    required String title,
+    required String description,
+    required List<Widget> actions,
+    required BuildContext context,
+  }) =>
+      showDialog<void>(
+        context: context,
+        builder: (context) => ErrorDialogWidget(
+          title: title,
+          description: description,
+          actions: actions,
+        ),
+        barrierDismissible: false,
+      );
 }
 
 final weatherResultViewModelProvider =
