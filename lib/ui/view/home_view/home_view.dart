cf3fe06a09cfd70da3227cf3f8fcd92de013f25f
YumNumm <onory0917@gmail.com>
2023-02-23 00:26:22 +0900

    add: backボタンの動作

--- lib/ui/view/home_view/home_view.dart
+++ lib/ui/view/home_view/home_view.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_training/ui/view/route.dart';
 
-class HomeView extends StatefulWidget {
+class HomeView extends ConsumerWidget {
   const HomeView({super.key});
 
   @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     WidgetsBinding.instance.endOfFrame.then(
       (_) async {
         await Future<void>.delayed(
           const Duration(milliseconds: 500),
         );
         // ↓ state.mountedでも良い
-        if (mounted) {
-          const WeatherResultViewRoute().go(context);
+        if (context.mounted) {
+          // 直近のRouteを取得
+          final router = ref.read(routerProvider);
+          // 既にWeatherResultViewに遷移している場合は何もしない
+          if (router.location == const WeatherResultViewRoute().location) {
+            return;
+          }
+          const WeatherResultViewRoute().push(context);
         }
       },
     );
-
     return const SizedBox.expand(
       child: DecoratedBox(
         decoration: BoxDecoration(
